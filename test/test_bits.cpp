8ff34832a7d09664f1f0446caf3d4f67e81258d3
Briar <72242853+QuestionableDeer@users.noreply.github.com>
2024-04-08 23:02:51 -0700

    add infrastructure for automated testing via Boost.Test

---
+++ test/test_bits.cpp
@@ -0,0 +1,17 @@
+#define BOOST_TEST_MODULE bit_tests
+#include <boost/test/included/unit_test.hpp>
+
+#include "bits.h"
+#include "types.h"
+
+BOOST_AUTO_TEST_CASE(low_byte_zero) {
+  Word tval = 0x0000;
+  Byte expect = 0x00;
+  BOOST_TEST(KeaBits::getLowByte(tval) == expect);
+}
+
+BOOST_AUTO_TEST_CASE(high_byte_zero) {
+  Word tval = 0x0000;
+  Byte expect = 0x00;
+  BOOST_TEST(KeaBits::getHighByte(tval) == expect);
+}
