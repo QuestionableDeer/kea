552ae78052f4fc2c913565c9c216785eb3e14cd3
Briar <72242853+QuestionableDeer@users.noreply.github.com>
2024-04-12 00:21:12 -0700

    fix oversight in existing test input format

--- test/test_bits.cpp
+++ test/test_bits.cpp
@@ -5,13 +5,13 @@
 #include "types.h"
 
 BOOST_AUTO_TEST_CASE(low_byte_zero) {
-  Word tval = 0x0000;
+  Word tval = 0xFF00;
   Byte expect = 0x00;
   BOOST_TEST(KeaBits::getLowByte(tval) == expect);
 }
 
 BOOST_AUTO_TEST_CASE(high_byte_zero) {
-  Word tval = 0x0000;
+  Word tval = 0x00FF;
   Byte expect = 0x00;
   BOOST_TEST(KeaBits::getHighByte(tval) == expect);
 }
